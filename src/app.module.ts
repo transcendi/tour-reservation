78589c4b90129dc1b0cea9f53ce1070c2c160793
Gerrard <gerrard@lazygourmet.club>
2023-11-28 02:55:24 +0900

    feat : generate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SellerModule } from './seller/seller.module';
+import { TourModule } from './tour/tour.module';
+import { CustomerModule } from './customer/customer.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [],
+  imports: [SellerModule, TourModule, CustomerModule, ReservationModule],
   controllers: [AppController],
   providers: [AppService],
 })
